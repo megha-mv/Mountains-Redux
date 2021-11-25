af1c54cd9c902481b2de493cdd44cd7b8942427f
Megha <meghvashist@gmail.com>
2021-11-25 18:04:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Container from './containers/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello from App</h1>
+      <Container/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Container.js
@@ -0,0 +1,13 @@
+import LandingPage from "../components/LandingPage";
+
+import React from 'react'
+
+const Container = () => {
+    return (
+        <div>
+            <LandingPage/>
+        </div>
+    )
+}
+
+export default Container;
